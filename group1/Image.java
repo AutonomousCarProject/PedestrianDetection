17751db73b14eb90d847b32f480da6ce3b218dc8
Spencer Hutchinson <Spencer Hutchinson>
2017-07-17 13:16:38 -0700

    Conversion method addded

--- group1/Image.java
+++ group1/Image.java
@@ -1,24 +1,18 @@
+package group1;
 
 class Image extends IImage{
 	int height;
 	int width;
+	int colorMargin = 30;
 	
 	Pixel[][] image;
 	
-	Image(){
+	Image(int width, int height){
 		for(int i = 0; i < height; i++) for(int j = 0; j < weight; j++){
 			image[i][j] = new Pixel();
 		}
-	}
-	
-	void convert(){	//converts rgb to yuv format
-		for(int i = 0; i < height; i++) for(int j = 0; j < weight; j++){
-			Pixel current = image[i][j];
-			short lum = current.red + current.green + current.blue;
-			lum = lum/765;
-			
-			
-		}
+		this.height = height;
+		this.width = width;
 	}
 	
 }
--- group1/Pixel.java
+++ group1/Pixel.java
@@ -1,35 +1,49 @@
+package group1;
+import java.lang.Math;
+
 	
-Pixel extends IPixel {	
+class Pixel implements IPixel {	//0-765
 	//YUV
 	
 	short luma;
-
     short saturation;
-
     short color;
 
     // RGB
     short red;
-
     short green;
-
     short blue;
 	
-	Pixel(short luma, short saturation, short color){
-		this.luma = luma;
-		this.saturation = saturation;
-		this.color = color;
-		red = null;
-		green = null;
-		blue = null;
-	}
-	
 	Pixel(short red, short green, short blue){
-		luma = null;
-		saturation = null;
-		color = null;
 		this.red = red;
 		this.green = green;
 		this.blue = blue;
+		convert();
+	}
+
+	private void convert(){
+		//luminance = sum of rgb
+		luma = red + green + blue;
+		
+		//saturation = max - min of rgb
+		saturation = (Math.max(Math.max(red, green), blue) - Math.min(Math.min(red, green), blue))*3;
+		
+		//hue
+		short ave = (red + green + blue);
+		int r = red*3;
+		int g = green*3;
+		int b = blue*3;
+		int rdiff = r - ave;
+		int gdiff = g - ave;
+		int bdiff = b - ave;
+		if(rdiff < colorMargin && gdiff < colorMargin && bdiff < colorMargin){	//if its not a distinct color
+			int topMargin = 765 - colorMargin;
+			if(r < colorMargin && g < colorMargin && b < colorMargin) color = 4;	//black
+			else if(r > topMargin && g > topMargin && b > topMargin) color = 5;	//white
+			else color = 3;
+		}
+		else if(rdiff > gdiff && rdiff > bdiff) color = 0;
+		else if(gdiff > rdiff && gdiff > bdiff) color = 1;
+		else if(bdiff > rdiff && bdiff > gdiff) color = 2;
 	}
 }
