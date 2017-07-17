d953c7c6c116c3919af19fdd0e5771f05efced40
Peter Looi <peterlooi11@gmail.com>
2017-07-17 11:35:36 -0700

    Made a new branch and are using an object to draw the lines around each person

---
+++ group5/IImageBoxDrawer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group5;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author peter_000
+ */
+public class IImageBoxDrawer 
+{
+    public void draw(IImage image, IBlobDetection iBlobs)
+    {
+        
+    }
+    protected Rectangle[] findRectangles(IImage image, IBlobDetection iBlobs)
+    {
+        //...
+    }
+    protected IImage drawLines(Point[] corners, IImage image, Color lineColor, int lineThickness)
+    {
+        //...
+    }
+            
+            
+    public class Point
+    {
+        private int x,y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int getX(){return x;}
+        public int getY(){return y;}
+    }
+    public class Rectangle
+    {
+        private Point[] points;
+        public Rectangle(Point[] points)
+        {
+            this.points = points;
+        }
+        public Point[] getPoints()
+        {
+            return points;
+        }
+    }
+    
+    
+}
