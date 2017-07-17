37730eb7b308a2101be3357e954b7968a464aa83
Jonathan Gordon <jonathan.adam.g@gmail.com>
2017-07-17 11:15:59 -0700

    Added BlobFilter class

---
+++ group4/BlobFilter.java
@@ -0,0 +1,14 @@
+public class BlobFilter implements IMovingBlobReduction 
+{
+	public List<MovingBlob> blobs;
+	
+	public BlobFilter(List<MovingBlob> blobs)
+	{
+		this.blobs = blobs;
+	}
+	
+	public List<MovingBlob> reduce(List<MovingBlob> blobs)
+	{
+		
+	}
+}
