416f0a91a9d5d739a84830e33a8ffbd85284d91c
Ben Mayinger <Mayngus@Barbaras-MacBook-Air.local>
2017-07-17 13:59:00 -0700

    Added more to file reader and conversion methods

--- group1/Image.java
+++ group1/Image.java
@@ -1,24 +1,18 @@
-
-<<<<<<< HEAD
-
 import java.io.*;
 import fly2cam.FlyCamera;
 
 public FlyCamera flyCam = new FlyCamera();
+public String fiName = "FlyCapped.By8";
 public byte[] camBytes = null;
 
+public Pixel[][] rgbPixels = null;
+public int pos = null;
 
 
-void readCam(){
-
-	flyCam.NextFrame(camBytes);
-
-}
-
-=======
 class Image extends IImage{
 	int height;
 	int width;
+	int frameRate = 15;
 	
 	Pixel[][] image;
 	
@@ -29,14 +23,44 @@ class Image extends IImage{
 	}
 	
 	void convert(){	//converts rgb to yuv format
+		
 		for(int i = 0; i < height; i++) for(int j = 0; j < weight; j++){
+			
 			Pixel current = image[i][j];
 			short lum = current.red + current.green + current.blue;
-			lum = lum/765;
-			
+			lum = lum/765;		
 			
 		}
 	}
+
+	void readCam(){
+
+		flyCam.Connect(frameRate);
+		flyCam.NextFrame(camBytes);
+
+	}
+
+	void byteConvert(){
+
+		pos = 0;
+
+		for(int i = 0 ; i < height * 2 ; =+ 2){
+
+
+			for(int j = 0 ; j < width * 2 ; j =+ 2){
+
+				rgbPixels[j/2][i/2].red = camBytes[pos];
+				rgbPixels[j/2][i/2].green = camBytes[pos + 1];
+				rgbPixels[j/2][i/2].blue = camBytes[pos + 1 + width * 2];
+				pos =+ 2;
+
+			}
+
+			pos + width * 2;
+
+		}
+
+	}
 	
 }
->>>>>>> 3adad10fdae8dbe47da94a3d948cde492bef5f23
+
