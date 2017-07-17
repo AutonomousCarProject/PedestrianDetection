87d312a68989d410c95b7840fcff2bcb5c59bb6e
William Adriance <williamadriance@Williams-MacBook-Pro.local>
2017-07-17 13:48:03 -0700

    Added constructor and ageOffScreen

--- group3/MovingBlob.java
+++ group3/MovingBlob.java
@@ -2,8 +2,15 @@ package group3;
 
 import group2.Blob;
 
-public class MovingBlob extends Blob
-{
-    public float movementX, movementY;
+public class MovingBlob extends Blob {
+    public float velocityX, velocityY;
     public int age;
+    public int ageOffScreen;
+    
+	public MovingBlob(){
+		this.velocityX = 0;
+		this.velocityY = 0;
+		this.age = 0;
+		this.ageOffScreen = 0;
+	}
 }
