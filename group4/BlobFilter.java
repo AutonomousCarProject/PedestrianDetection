9059bf78076c75ab10404fe39ec71e85dc6f725f
Jonathan Gordon <jonathan.adam.g@gmail.com>
2017-07-17 14:09:03 -0700

    Added documentation

--- group4/BlobFilter.java
+++ group4/BlobFilter.java
@@ -7,18 +7,32 @@ public class BlobFilter implements IMovingBlobReduction
 		this.blobs = blobs;
 	}
 	
+	/**
+	 * Checks the list of potential pedestrian blobs to distinguish pedestrians from non-pedestrians.
+	 * Non-pedestrians are removed from the list of blobs.
+	 * 
+	 * @param blobs 	the list of potential pedestrian blobs
+	 * @return			the list of blobs determined to be pedestrians
+	 */
 	public List<MovingBlob> reduce(List<MovingBlob> blobs)
 	{
 		for (int i = 0; i < blobs.length(); i++)
 		{
-			if (!isPerson(blobs.get(i)))
+			if (!isPedestrian(blobs.get(i)))
 			{
 				blobs.remove(i--);
 			}
 		}
 	}
 	
-	public boolean isPerson(MovingBlob blob)
+	/**
+	 * Checks an individual blob to determine if it is a pedestrian or non-pedestrian. Determination
+	 * is made based on blob width vs. height, blob 'age' on screen, and blob xVelocity.
+	 * 
+	 * @param blob 		the blob being checked
+	 * @return 			if the blob is a pedestrian
+	 */
+	public boolean isPedestrian(MovingBlob blob)
 	{
 		if (blob.width > blob.height) return false;
 		if (blob.age < 10) return false;
