85503ced47ef83b3ebf65cbb8f96f36c3f0e7566
Spencer Hutchinson <Spencer Hutchinson>
2017-07-18 13:11:28 -0700

    Updated Image

--- group1/Image.java
+++ group1/Image.java
@@ -7,24 +7,15 @@ import group1.fly0cam.FlyCamera;
 //Defines image as an 2d array of pixels
 class Image implements IImage{
 	
-	private int height;
-	private int width;
+	public static final int height = 480;
+	public static final int width = 640;
 
 	private int frameRate = 3;
 	private FlyCamera flyCam = new FlyCamera();
 	
 	//307200
 	private byte[] camBytes = new byte[2457636];
-	private IPixel[][] image;
-
-	Image(int width, int height){
-
-		this.height = height;
-		this.width = width;
-		image = new Pixel[width][height];
-
-
-	}
+	private IPixel[][] image = new Pixel[width][height];
 
 	
 	public IPixel[][] getImage(){
@@ -52,14 +43,11 @@ class Image implements IImage{
 	private void byteConvert(){
 
 		int pos = 0;
-		System.out.println("Position: " + pos);
 
 		for(int i = 0 ; i < height * 2 ; i += 2){
 
-
 			for(int j = 0 ; j < width * 2 ; j += 2){
-
-				System.out.println(camBytes[pos]+"  "+camBytes[pos+1]+"  "+camBytes[pos + 1 + width * 2]);
+				
 				image[j/2][i/2] = new Pixel((short)(camBytes[pos]+128), (short)(camBytes[pos + 1]+128), (short)(camBytes[pos + 1 + width * 2]+128));
 
 				pos += 2;
