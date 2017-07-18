d024a17ceec8fd135ee15637038abc5cce605ed8
William Adriance <williamadriance@Williams-MacBook-Pro.local>
2017-07-18 09:15:55 -0700

    created file solely for testing data

--- group3/MovingBlobDetectionTest.java
+++ group3/MovingBlobDetectionTest.java
@@ -3,19 +3,20 @@ import group2.Blob;
 import java.util.LinkedList;
 import java.util.List;
 
-public class MovingBlobDetection implements IMovingBlobDetection {
+public class MovingBlobDetectionTest implements IMovingBlobDetection {
 	private List<MovingBlob> movingBlobs;
 	
-	public MovingBlobDetection() {
+	public MovingBlobDetectionTest() {
 		movingBlobs = new LinkedList<>();
 	}
 	
+	// test data
 	public List<MovingBlob> getMovingBlobs(List<Blob> blobList){
-		updateMovingBlobs(blobList);
+		for (Blob blob:blobList){
+			this.movingBlobs.add(new MovingBlob());
+		}
 		return movingBlobs;
 	}
-	
-	// test data in "MovingBlobDetectionTest.java"
 	
 	private void updateMovingBlobs(List<Blob> blobList){
 		for(Blob blob:blobList){
