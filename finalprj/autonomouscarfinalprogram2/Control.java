f6c5cff87a48833f09472afeabfe7df690fc2937
Peter Looi <peterlooi11@gmail.com>
2017-07-19 10:37:43 -0700

    Final project updated

--- finalprj/autonomouscarfinalprogram2/Control.java
+++ finalprj/autonomouscarfinalprogram2/Control.java
@@ -6,7 +6,16 @@
 package autonomouscarfinalprogram2;
 
 import com.looi.looi.LooiObject;
-import java.awt.List;
+import group1.Image;
+import group2.Blob;
+import group2.BlobDetection;
+import group3.MovingBlob;
+import group3.MovingBlobDetection;
+import group4.BlobFilter;
+import group5.IImageBoxDrawer;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -19,24 +28,48 @@ public class Control extends LooiObject
     private BlobFilter blobFilter;
     private IImageBoxDrawer boxDrawer;
     private Image currentImage;
+    private BufferedImage testBI = new BufferedImage(10,10,BufferedImage.TYPE_INT_ARGB);
+    {
+        for(int r = 0; r < testBI.getHeight(); r++)
+        {
+            for(int c = 0; c < testBI.getWidth(); c++)
+            {
+                int p = (255/*alpha*/ << 24) | (255 << 16) | (255 << 8) | 255;
+                testBI.setRGB(c,r,p);
+            }
+        }
+    }
     public Control()
     {
         blobDetection = new BlobDetection();
         movingBlobDetection = new MovingBlobDetection();
         blobFilter = new BlobFilter();
-        currentImage = new Image(640,480);
+        currentImage = new Image();
+        boxDrawer = new IImageBoxDrawer();
     }
     protected void looiStep()
     {
         currentImage.readCam();
+        
         List<Blob> knownBlobs = blobDetection.getBlobs(currentImage);
+        
         List<MovingBlob> movingBlobs = movingBlobDetection.getMovingBlobs(knownBlobs);
+        
         List<MovingBlob> filteredBlobs = blobFilter.reduce(movingBlobs);
         boxDrawer.draw(currentImage,filteredBlobs);
+        
+        /*for(int r = 0; r < boxDrawer.getCurrentImage().getHeight(); r++)
+        {
+            for(int c = 0; c < boxDrawer.getCurrentImage().getWidth(); c++)
+            {
+                System.out.println(boxDrawer.getCurrentImage().getRGB(c,r));
+            }
+        }*/
         
     }
     protected void looiPaint()
     {
         drawImage(boxDrawer.getCurrentImage(),0,0,getInternalWidth(),getInternalHeight());
+        //drawImage(testBI,0,0,getInternalWidth(),getInternalHeight());
     }
 }
