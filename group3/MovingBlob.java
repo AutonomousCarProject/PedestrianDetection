f7701183afb5165647c30640b3bdfa41e99a4678
William Adriance <williamadriance@Williams-MacBook-Pro.local>
2017-07-19 09:28:28 -0700

    documentation has been added

--- group3/MovingBlob.java
+++ group3/MovingBlob.java
@@ -3,12 +3,25 @@ package group3;
 import group2.Blob;
 
 public class MovingBlob extends Blob {
+	
+	// The X and Y components of the MovingBlob's velocity.
     public float velocityX, velocityY;
+    
+	// The predicted X and Y coordinates of the MovingBlob in the next frame.
     public float predictedX, predictedY;
     
+    // The time, in frames, that the MovingBlob has been on-screen.
     public int age;
+    
+    // The time, in frames, that the MovingBlob has been off-screen.
     public int ageOffScreen;
 	
+    /**
+     * Creates a MovingBlob from a Blob. This is used when a previously unseen Blob
+     * comes on-screen.
+     * 
+     * @param b The Blob that the MovingBlob will be created from.
+     */
 	public MovingBlob(Blob b){
 		super(b.width, b.height, b.centerX, b.centerY, b.color);
 		this.velocityX = 0;
@@ -18,6 +31,10 @@ public class MovingBlob extends Blob {
 		updatePredictedPosition();
 	}
 	
+	/**
+	 * Updates the predicted position of the MovingBlob based on its center and
+	 * velocity.
+	 */
 	public void updatePredictedPosition(){
 		predictedX = velocityX + centerX;
 		predictedY = velocityY + centerY;
