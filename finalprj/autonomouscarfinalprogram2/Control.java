f0f109175cb348926ccd7ec69f7a71711d37430e
SMCCoder <sofie.christie@gmail.com>
2017-07-27 14:02:01 -0700

    synced blobs with pause

--- finalprj/autonomouscarfinalprogram2/Control.java
+++ finalprj/autonomouscarfinalprogram2/Control.java
@@ -197,11 +197,13 @@ public class Control extends LooiObject
     public void updateWhilePaused(){
 		currentImage.setImage(frameList.get(currentFrame));
 		
-        List<MovingBlob> movingBlobs = movingBlobDetection.getMovingBlobs();
+		List<Blob> knownBlobs = blobDetection.getBlobs(currentImage);
+        List<MovingBlob> movingBlobs = movingBlobDetection.getMovingBlobs(knownBlobs);
         List<MovingBlob> fmovingBlobs = blobFilter.filterMovingBlobs(movingBlobs);
-		//boxDrawer.draw2(currentImage, movingBlobs, fmovingBlobs);
-        boxDrawer.draw(currentImage, movingBlobs);
-
+        List<MovingBlob> unifiedBlobs = movingBlobDetection.getUnifiedBlobs(fmovingBlobs);
+        List<MovingBlob> funifiedBlobs = blobFilter.filterUnifiedBlobs(unifiedBlobs);
+        //boxDrawer.draw2(currentImage,fmovingBlobs,funifiedBlobs);
+        boxDrawer.draw(currentImage, funifiedBlobs);
 	        
     }   
     
