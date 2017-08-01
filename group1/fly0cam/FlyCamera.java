c8135644d6219b16b38c189b2d8c0b5f93ebff00
SMCCoder <sofie.christie@gmail.com>
2017-08-01 09:06:38 -0700

    no change

--- group1/fly0cam/FlyCamera.java
+++ group1/fly0cam/FlyCamera.java
@@ -9,9 +9,9 @@ public class FlyCamera {
 
 
 //  private final String fiName = "group1/fly0cam/FlyCapped.By8";  // --non IDE
-//private final String fiName = "src/group1/fly0cam/FlyCapped8.By8"; // -- IDE
+//private final String fiName = "group1/fly0cam/FlyCapped8.By8"; // -- IDE
 //  private final String fiName = "src/group1/fly0cam/.By8";
-  private final String fiName = "src/group1/fly0cam/Mix_0_0_0.By8"; // -- IDE
+  private final String fiName = "group1/fly0cam/FlyCapped7.By8"; // -- IDE
 
 
   public int rose, // actual number of rows = FlyCap2.fc2Image.rows/2
