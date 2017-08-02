460a0a58521158043a861109cbac749fa6b4442e
plooi <peterlooi11@gmail.com>
2017-08-02 10:26:30 -0700

    Update IImageDrawing.java

--- group5/IImageDrawing.java
+++ group5/IImageDrawing.java
@@ -1,12 +1,12 @@
 package group5;
 
 import group1.IImage;
-import group2.IBlobDetection;
-import group3.MovingBlob;
+import group2.Blob;
 
 import java.util.List;
 
 public interface IImageDrawing
 {
-    void draw(IImage image, List<? extends Blob> iBlobs);
+    void blobsToRectangles(IImage image, List<? extends Blob> iBlobs);
+    void draw(IImage image);
 }
